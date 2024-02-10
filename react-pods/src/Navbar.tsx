1c01f86f29d2a8c43b371b2df6303055da689ac3
brunscii <brunscii@gmail.com>
2024-02-09 21:37:12 -0500

    started small react app with vite

---
+++ react-pods/src/Navbar.tsx
@@ -0,0 +1,20 @@
+import { ReactComponentElement } from "react";
+
+function Navbar(){
+
+    return (
+    <>
+        <div className='navbar'>
+            <ul>
+                <li>Home</li>
+                <li>Pods</li>
+                <li>RSS</li>
+                <li>View</li>
+                <li>Settings</li>
+            </ul>       
+        </div>
+    </>)
+
+}
+
+export default Navbar
