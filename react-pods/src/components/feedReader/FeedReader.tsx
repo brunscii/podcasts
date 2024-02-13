193dd6b2b4346a0e51a808e8e22f5a526e988ef1
Chris Carlin <brunscii@gmail.com>
2024-02-12 23:21:11 -0500

    added onchange to input

--- react-pods/src/components/feedReader/FeedReader.tsx
+++ react-pods/src/components/feedReader/FeedReader.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import "./FeedReader.css"
+import RSSInfoSection from "./RSSInfoSection"
+
 function FeedReader() {
+
+  const [inputUrl, setInputUrl] = useState('');
+
   return (
     <>
     <div className='rss-input-box' >
       <div className="rss-input">
         <label htmlFor="rss-input">Enter a RSS URL</label>
-        <input title="rss-input" type="url" name="rss-input" id="rss-input" placeholder="rss.website.com" />
+        <input title="rss-input" type="url" name="rss-input" id="rss-input" placeholder="rss.website.com" value={inputUrl} 
+          onSubmit={ (e)=>{
+            e.preventDefault();
+
+            // test if a valid rss or not
+            // Read in RSS from inputUrl's address
+          
+          }}
+
+          onChange={(e)=>{
+            setInputUrl(e.target.value)
+          }}
+        />
+      </div>
+      <div className="rss-content">
+        
       </div>
     </div>
     </>
---
+++ react-pods/src/components/feedReader/RSSInfoSection.tsx
@@ -0,0 +1,28 @@
+interface RSSInfoSectionProps{
+  title: string;
+  description: string;
+  imageUrl: string;
+  episodes?: {
+    title: string;
+    pubDate: string;
+    link: string;
+  }[];
+}
+
+
+function RSSInfoSection( props : RSSInfoSectionProps ){
+
+  return (
+    <>
+    <div className="RSSInfoBox">
+      <h3>{props.title}</h3>
+      <span>{props.description}</span>
+      <img src={props.imageUrl} alt="" />
+
+    </div>
+    </>
+  )
+
+}
+
+export default RSSInfoSection
