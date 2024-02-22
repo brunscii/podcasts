ac3ab7f716a2bee879e541e45e440064b33323b1
brunscii <brunscii@gmail.com>
2024-02-21 21:27:21 -0500

    got api get working

--- node-pods/dist/index.js
+++ node-pods/dist/index.js
@@ -4,10 +4,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const csv_parse_1 = require("csv-parse");
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
+console.log(path_1.default.join(__dirname, 'podcasts.csv'));
+function readPods() {
+    const parser = (0, csv_parse_1.parse)({ 'delimiter': ', ', columns: true });
+    const readStream = fs_1.default.createReadStream(path_1.default.join(__dirname, 'podcasts.csv')).pipe(parser);
+    let csv = [];
+    readStream.on('readable', () => {
+        let data = readStream.read();
+        while (data != null) {
+            csv.push({ name: data.name,
+                link: data.link });
+            console.log(data.link);
+            data = readStream.read();
+        }
+    });
+    console.log('asdf', csv);
+    return csv;
+}
+const pods = readPods();
+app.get('/', (_req, res) => {
     res.send('Hello World!');
+});
+app.get('/api', (req, res) => {
+    console.log('requested api');
+    // let podDiv = ''
+    // for( let p of pods ){
+    //   podDiv = podDiv + `<a href=${p.link}>${p.link}</a><br/>`
+    //   console.log(p)
+    // }
+    // // console.log(pods)
+    // res.send(podDiv)
+    res.send(pods);
 });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
--- node-pods/src/index.tsx
+++ node-pods/src/index.tsx
@@ -1,9 +1,54 @@
 import express from 'express';
+import path from 'path';
+import {parse} from 'csv-parse';
+import fs from 'fs';
 const app = express();
 const port = 3000;
+console.log(path.join(__dirname, 'podcasts.csv'))
 
-app.get('/', (req, res) => {
+interface podData {
+  name: string,
+  link: string
+}
+
+function readPods() {
+  const parser  = parse( {'delimiter': ', ', columns: true} )
+  const readStream = fs.createReadStream( path.join(__dirname, 'podcasts.csv') ).pipe(parser);
+  let csv : podData[] = []
+  readStream.on('readable',()=>{
+    let data = readStream.read()
+    while(data != null){
+      csv.push( {name: data.name,
+                 link: data.link} )
+      console.log(data.link)
+      data = readStream.read()
+    }
+  })
+  console.log('asdf',csv)
+  return csv
+
+}
+
+const pods = readPods()
+
+app.get('/', (_req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => {
+  console.log('requested api')
+
+  
+  
+  // let podDiv = ''
+  // for( let p of pods ){
+  //   podDiv = podDiv + `<a href=${p.link}>${p.link}</a><br/>`
+  //   console.log(p)
+  // }
+  // // console.log(pods)
+  // res.send(podDiv)
+  res.send(pods)
+
 });
 
 app.listen(port, () => {
