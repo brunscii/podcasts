808a77370a44d3cd78fb35f070037c756061df73
brunscii <brunscii@gmail.com>
2024-03-10 14:12:01 -0400

    made home show list of subbed podcasts

--- react-pods/src/components/home/PodcastScrollableList.tsx
+++ react-pods/src/components/home/PodcastScrollableList.tsx
@@ -1,6 +1,22 @@
-function PodcastScrollableList(){
 
-  return <></>
+interface PodcastScrollableListProps{
+  podcasts: {
+    name: string;
+    link: string;
+  }[]
+}
+
+
+function PodcastScrollableList( props?: PodcastScrollableListProps ){
+  console.log(props?.podcasts)
+  return <>
+  {props?.podcasts.map( (pod)=>{
+    <div className='podcast-agg-list-item'>
+      <span>{pod.name}</span><span>{pod.link}</span>
+    </div>
+  } 
+  )}
+  </>
 }
 
 export default PodcastScrollableList
